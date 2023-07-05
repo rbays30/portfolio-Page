786dba1074a3815917b2575c8926ea2af64029d1
rbays30 <lbays30@gmail.com>
2023-07-05 12:29:39 -0400

    updating config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rbays30.github.io/',
+  base: '/portfolio-Page/',
   plugins: [
     vue(),
   ],
